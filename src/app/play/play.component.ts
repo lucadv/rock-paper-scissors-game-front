9e413fc8961beccfcb2b2687af1f4fe6a0abb3a0
Luca Di Vincenzo <luca.divincenzo@piksel.com>
2018-08-31 11:22:43 +0200

    moving playableShapes and shape up one lvl

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { Shape } from './shape';
-import { PLAYABLESHAPES } from './playableShapes';
+import { Shape } from '../shape';
+import { PLAYABLESHAPES } from '../playableShapes';
 
 @Component({
   selector: 'app-play',
