cc6476f5ad7169a87da835a7fc274bee3d437612
Luca Di Vincenzo <luca.divincenzo@piksel.com>
2018-09-06 08:08:16 +0200

    moving opponent selection in the nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { OpponentSelectorComponent } from './opponent-selector/opponent-selector.component';
 import { ShapeSelectorComponent } from './shape-selector/shape-selector.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { MessagesComponent } from './messages/messages.component';
@@ -15,7 +14,6 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    OpponentSelectorComponent,
     ShapeSelectorComponent,
     MessagesComponent,
     CountersComponent,
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Opponent } from '../opponent';
+import { MessageService } from '../message.service';
+import { SELECTABLE_OPPONENTS } from '../selectableOpponents';
 
 @Component({
   selector: 'app-nav',
@@ -9,9 +12,19 @@ export class NavComponent implements OnInit {
 
   title = 'Rock, paper, scissors game';
   
-  constructor() { }
+  opponents = SELECTABLE_OPPONENTS;
 
+  selectedOpponent: Opponent;
+
+  constructor(private messageService: MessageService) { }
+  
   ngOnInit() {
   }
+
+  onSelect(opponent: Opponent): void {
+    this.messageService.add(`Selected ${opponent.name} opponent`);
+    this.selectedOpponent = opponent;
+  }
+
 
 }
